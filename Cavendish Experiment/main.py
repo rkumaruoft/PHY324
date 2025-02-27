85d49ad1c2840d0456180c309ee837883ab32ef3
reesh <reeshav.kumar@mail.utoronto.ca>
2025-02-27 02:17:46 -0500

    small chzn

---
+++ Cavendish Experiment/main.py
@@ -0,0 +1,11 @@
+import math
+import pickle
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import numpy as np
+from scipy.optimize import curve_fit
+from scipy.stats import chi2
+import csv
+
+if __name__ == "__main__":
+    print("Hello")
